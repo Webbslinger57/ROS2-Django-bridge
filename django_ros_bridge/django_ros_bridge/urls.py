eb7b3fec5ddc9ca93b0b29d160872468ca28da14
elijah <webbslinger57@gmail.com>
2024-01-15 14:26:18 -0600

    updated launch script

--- django_ros_bridge/django_ros_bridge/urls.py
+++ django_ros_bridge/django_ros_bridge/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from bridge_api.views import ROS2APIView, ROS2TopicAPIView, ROS2ServiceAPIView
+from bridge_api.views import ROS2APIView #, ROS2TopicAPIView, ROS2ServiceAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- django_ros_bridge/launch.sh
+++ django_ros_bridge/launch.sh
@@ -3,8 +3,7 @@ echo "Sourcing ROS2..."
 source /opt/ros/humble/setup.bash
 echo "Sourcing ros2 workspace..."
 source $HOME/ros2_ws/install/setup.bash
-
-echo "Starting django_ros_bridge..."
+echo "===================================================="
 echo " ____      ____     _      __   __   _____    ____  "
 echo "|  _ \     |  |    / \    |  \ |  | /  ___|  / __ \ "
 echo "| | | |    |  |   / - \   |   \|  ||  /     | |  | |"
@@ -12,4 +11,5 @@ echo "| | | | _  |  |  /  _  \  |    \  || |   __ | |  | |"
 echo "| |_| || |_|  | /  / \  \ |  |\   ||  \_|_ || |__| |"
 echo "|____/ |______|/__/   \__\|__| \__| \_____/  \____/ "
 echo "===================================================="
-source python3 manage.py runserver 0.0.0.0:8000
+echo "Starting django_ros_bridge..."
+python3 manage.py runserver 0.0.0.0:8000
