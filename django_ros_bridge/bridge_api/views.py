749cac3fb896d220c9bf6883eade63440cf693d2
elijah <webbslinger57@gmail.com>
2024-01-15 11:26:35 -0600

    added get nodes

--- django_ros_bridge/bridge_api/views.py
+++ django_ros_bridge/bridge_api/views.py
@@ -11,16 +11,20 @@ class ROS2APIView(APIView):
     def get(self, request, format=None):
         rclpy.init(args=None)
         node = ROS2Node()
-
+        
+        # Get a list of all nodes
+        nodes = node.get_node_names()
         # Get a list of all topics
         topics = node.get_topic_names_and_types()
         # Get a list of all services
         services = node.get_service_names_and_types()
+        
 
         node.destroy_node()
         rclpy.shutdown()
 
         return Response({
+            'nodes': nodes,
             'topics': topics,
             'services': services,
         })
