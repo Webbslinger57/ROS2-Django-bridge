eda4fe52cba2f41bd70c7cf9becf595232b04bc1
elijah <webbslinger57@gmail.com>
2024-01-15 11:23:06 -0600

    Added rest framework

--- django_ros_bridge/bridge_api/views.py
+++ django_ros_bridge/bridge_api/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+import rclpy
+from rclpy.node import Node
 
-# Create your views here.
+class ROS2Node(Node):
+    def __init__(self):
+        super().__init__('ros2_node')
+
+class ROS2APIView(APIView):
+    def get(self, request, format=None):
+        rclpy.init(args=None)
+        node = ROS2Node()
+
+        # Get a list of all topics
+        topics = node.get_topic_names_and_types()
+        # Get a list of all services
+        services = node.get_service_names_and_types()
+
+        node.destroy_node()
+        rclpy.shutdown()
+
+        return Response({
+            'topics': topics,
+            'services': services,
+        })
