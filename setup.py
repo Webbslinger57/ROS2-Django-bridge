105154f505654e8b888150b7fae8bd438d07075e
elijah <webbslinger57@gmail.com>
2024-01-15 09:23:07 -0600

    first commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'django_ros2_bridge'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='elijah',
+    maintainer_email='webbslinger57@gmail.com',
+    description='This package contains a Django app that provides a web interface for ROS2.',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
