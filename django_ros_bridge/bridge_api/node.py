c81840cb8a9ac931067bae700963f28e2b6b14be
elijah <webbslinger57@gmail.com>
2024-01-16 13:32:38 -0600

    Get JPEG instead of entire image

--- django_ros_bridge/bridge_api/node.py
+++ django_ros_bridge/bridge_api/node.py
@@ -1,7 +1,10 @@
 import queue
 import threading
+import time
 import rclpy
 from rclpy.node import Node
+from std_msgs.msg import Header
+from sensor_msgs.msg import Image
 import importlib
 
 """
@@ -12,7 +15,7 @@ class ROS2Node(Node):
     # Initialize the ROS2 node.
     def __init__(self):
         super().__init__('django_bridge_node')
-        self.incoming_msgs = queue.Queue(maxsize=100)
+        self.incoming_msgs = queue.Queue(maxsize=20)
         self.spin_node_thread = threading.Thread(target=self.spin_node)
         self.spin_node_thread.start()
         
@@ -21,6 +24,7 @@ class ROS2Node(Node):
         try:
             while rclpy.ok():
                 rclpy.spin_once(self)
+                time.sleep(0.5)
                 # Process messages...
         except rclpy.executors.ExternalShutdownException:
             pass
@@ -40,7 +44,6 @@ class ROS2Node(Node):
         
     # Handles the callback for a subscription
     def sub_callback(self, msg):
-        print(msg)
         if self.incoming_msgs.full():
             self.incoming_msgs.get()
         self.incoming_msgs.put(msg)
@@ -49,20 +52,30 @@ class ROS2Node(Node):
     def add_subscription(self, topic_endpoint, msg_type):
         type = self.get_message_type(msg_type)
         self.create_subscription(type, topic_endpoint, self.sub_callback, 10)
-        print(self.subscriptions)
         
     # Make a service call and return the response
     def call_service(self, service_endpoint, msg_type, msg):
         type = self.get_message_type(msg_type)
         print(type)
        
+    def header_to_dict(self, header):
+        return {
+            'stamp': header.stamp.sec + header.stamp.nanosec / 1e9,
+            'frame_id': header.frame_id
+        }
+       
     # Convert a message to a dictionary
     def msg_to_dict(self, msg):
         msg_dict = {}
         for field_name in msg.get_fields_and_field_types():
-            field_value = getattr(msg, field_name)
+            if field_name == 'data' and isinstance(msg, Image):
+                field_value = 'JPEG Image'
+            else:
+                field_value = getattr(msg, field_name)
+            if isinstance(field_value, Header):
+                field_value = self.header_to_dict(field_value)
             msg_dict[field_name] = field_value
-        return msg_dict 
+        return msg_dict
         
     # Get the imcoming messages
     def get_incoming_msgs(self):
