3d8a627144f70f611a84cc26f83da52a7777c77a
elijah <webbslinger57@gmail.com>
2024-01-16 09:25:28 -0600

    Dynamically gets topics and types

--- django_ros_bridge/bridge_api/views.py
+++ django_ros_bridge/bridge_api/views.py
@@ -1,8 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
 import rclpy
-from rclpy.node import Node
-from std_msgs.msg import String
 from bridge_api.node import ROS2Node
 
 """
@@ -18,7 +17,6 @@ all ROS2 nodes, topics, and services as well as their types.
 """
 class ROS2APIView(APIView):
     def get(self, request, format=None):
-        
         # Get a list of all nodes
         nodes = node.get_node_names()
         # Get a list of all topics
@@ -33,17 +31,31 @@ class ROS2APIView(APIView):
         })
 
 """
-This view is used to get the latest message from a
-given ROS2 topic.
+This view gets information about a specific ROS2 topic.
 """
 class ROS2TopicAPIView(APIView):    
     def get(self, request, format=None):
         topic_endpoint = request.GET.get('topic_endpoint', None)
         
+        # Get a list of all topics
+        topics = node.get_topic_names_and_types()
+        
+        # Create a dictionary where the keys are the topic names and the values are the types
+        topic_dict = {topic[0]: topic[1] for topic in topics}
+        
+        if topic_endpoint not in topic_dict:
+            return Response({
+                'status': '400',
+                'error': 'Topic not found.',
+            })
+            
+        type = topic_dict[topic_endpoint]
+        
+        return Response({
+            'status': '200',
+            'topic': topic_endpoint,
+            'message_type': type,
+        })
+        
         # Add Subscription
         # node.add_subscription(topic_endpoint, 'std_msgs/msg/String')
-
-        return Response({
-            'topic': topic_endpoint,
-            'message_type': 'std_msgs/msg/String',
-        })
