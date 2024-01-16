6bdce58e19a323cf9c7b0f2982be043ff2a4a9ee
elijah <webbslinger57@gmail.com>
2024-01-16 09:35:50 -0600

    Added dynamic imports

--- django_ros_bridge/bridge_api/node.py
+++ django_ros_bridge/bridge_api/node.py
@@ -1,7 +1,6 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
-from std_msgs.msg import Int32
+import importlib
 
 """
 This class is a ROS2 node that is used to get a list of all
@@ -10,31 +9,24 @@ ROS2 nodes, topics, and services as well as their types.
 class ROS2Node(Node):
     # Initialize the ROS2 node.
     def __init__(self):
-        self.msg_types = { # Dictionary of message types and their callbacks
-            'std_msgs/msg/String': [String, self.str_callback],
-            'std_msgs/msg/Int32': [Int32, self.int_callback],
-        }
-        self._subscriptions = {}
-        self._publishers = {}
         super().__init__('django_bridge_node')
         
     def deinit(self):
         self.node.destroy_node()
         rclpy.shutdown()
         
-    # Handles string subscriptions
-    def str_callback(self, msg):
-        print(msg.data)
-        return msg.data
-
-    # Handles int subscriptions
-    def int_callback(self, msg):
-        print(msg.data)
-        return msg.data
+    def get_message_type(message_type_str):
+        module_name, message_name = message_type_str.replace('/', '.').rsplit('.', 1)
+        module = importlib.import_module(module_name)
+        return getattr(module, message_name)
+        
+    # Handles the callback for a subscription
+    def sub_callback(self, msg):
+        print(msg)
+        return msg
 
     # Adds a subscription to the node
     def add_subscription(self, topic_endpoint, msg_type):
-        type = self.msg_types[msg_type][0]
-        callback = self.msg_types[msg_type][1]
-        self.create_subscription(type, topic_endpoint, callback, 10)
+        type = self.get_message_type(msg_type)
+        self.create_subscription(type, topic_endpoint, self.sub_callback, 10)
         print(self.subscriptions)
